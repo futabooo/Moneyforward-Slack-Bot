8be25e3df0094196e41f7a518226948dd8799b4f
futabooo <takahiro.futagawa@gmail.com>
2021-04-04 18:34:11 +0900

    fix

--- src/app.ts
+++ src/app.ts
@@ -24,33 +24,81 @@ slackApp.message('ping', async ({ say }) => {
   say('pong')
 });
 
-slackApp.command('/moneyforward', async ({ command, ack, context }) => {
-  console.log("[receive] サマリーくれ with slach command");
+slackApp.command('/moneyforward', async ({ command, ack, say, context }) => {
   await ack();
 
-  // 3秒以上かかるのでSlackにエラーが表示されたあと画像が返ってくる
-  try {
-    const image = await SpendingSummariesImage();
-    await slackApp.client.files.upload({
-      token: context.botToken,
-      channels: command.channel_id,
-      file: image,
-    });
-  } catch (err) {
-    console.log(err)
+  const commandText = command.text.toLowerCase();
+  const commandInputs = commandText ? commandText.split(/\s+/) : [];
+  const mode = commandInputs.length > 0 ? commandInputs[0] : 'budget';
+
+  switch (mode) {
+    case 'budget':
+      try {
+        // CloudRunのCold Startに当たると3秒以上かかるのでSlackにエラーが表示されたあと画像が返ってくる
+        const image = await budgetCapture();
+        await slackApp.client.files.upload({
+          token: context.botToken,
+          channels: command.channel_id,
+          file: image,
+        });
+      } catch (err) {
+        console.log(err)
+        say(`画像の取得に失敗しました`);
+      }
+      break;
+    case 'actual':
+      const today = new Date();
+      const yearMonth = commandInputs.length > 1 ? commandInputs[1] : '';
+      const year = yearMonth.length > 0 ? yearMonth.substr(0, 4) : today.getFullYear().toString();
+      const month = yearMonth.length > 0 ? yearMonth.substr(4, 2).replace('0', '') : (today.getMonth() + 1).toString();
+      try {
+        // CloudRunのCold Startに当たると3秒以上かかるのでSlackにエラーが表示されたあと画像が返ってくる
+        const image = await actualCapture(year, month);
+        await slackApp.client.files.upload({
+          token: context.botToken,
+          channels: command.channel_id,
+          file: image,
+        });
+      } catch (err) {
+        console.log(err)
+        say(`画像の取得に失敗しました`);
+      }
+      break;
+    default:
+      say(`${mode}は対応していないコマンドです`);
   }
 });
 
-async function SpendingSummariesImage(): Promise<Buffer | undefined> {
+async function budgetCapture(): Promise<Buffer | undefined> {
   const browser = await createBrowser();
   const page = await browser.newPage();
-  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
+  await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36');
   await openLogin(page);
   await login(page, mailAddress, password);
   await page.waitForNavigation();
-  await openSpendingSummaries(page, groupId);
+  await selectGroup(page, groupId);
+  await openSpendingSummaries(page);
   await page.waitForTimeout(2000); // FIXME: グラフが表示されるのを待つ
-  const element = await page.$('#main-container');//summary-info-content
+  const element = await page.$('#main-container');
+  const imageBuffer = await element?.screenshot({ encoding: 'binary' });
+  await browser.close;
+  return imageBuffer;
+}
+
+async function actualCapture(year: string, month: string): Promise<Buffer | undefined> {
+  const browser = await createBrowser();
+  const page = await browser.newPage();
+  await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36');
+  await openLogin(page);
+  await login(page, mailAddress, password);
+  await page.waitForNavigation();
+  await selectGroup(page, groupId);
+  await openCf(page);
+  await page.$eval(`[data-year="${year}"] [data-month="${month}"]`, el => (el as HTMLElement).click());
+  await page.waitForTimeout(2000); // FIXME: 月を選択したあと表示が完了するのを待つ
+  await openCfSummary(page);
+  await page.waitForTimeout(2000); // FIXME: グラフが表示されるのを待つ
+  const element = await page.$('#summary-info-content');
   const imageBuffer = await element?.screenshot({ encoding: 'binary' });
   await browser.close;
   return imageBuffer;
@@ -100,6 +148,11 @@ async function login(page: Page, mailAddress: string, password: string) {
   await page.click('input[type="submit"]');
 }
 
+async function selectGroup(page: Page, groupId: string) {
+  await page.select('select#group_id_hash', groupId)
+  await page.waitForSelector('.alert-success');
+}
+
 async function openLogin(page: Page) {
   await page.goto(baseUrl);
   await Promise.all([
@@ -108,8 +161,17 @@ async function openLogin(page: Page) {
   ]);
 }
 
-async function openSpendingSummaries(page: Page, groupId: string) {
+async function openSpendingSummaries(page: Page) {
   await page.goto(`${baseUrl}/spending_summaries`);
   await page.waitForSelector('#page-spending-summaries');
-  await page.select('select#group_id_hash', groupId)
+}
+
+async function openCf(page: Page) {
+  await page.goto(`${baseUrl}/cf`);
+  await page.waitForSelector('#calendar');
+}
+
+async function openCfSummary(page: Page) {
+  await page.goto(`${baseUrl}/cf/summary`);
+  await page.waitForSelector('#cf-summary');
 }
